469c1c04b0d4362bc9414af322b5794f8fa9155f
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-11-07 23:50:43 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan44-HukumOhm/src/pboif2/pkg10118062/latihan44/hukumohm/PBOIF210118062Latihan44HukumOhm.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan44.hukumohm;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBOIF210118062Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Baterai hasil = new Baterai();
+        hasil.getKuatArus();
+        hasil.getHambatan(); 
+        hasil.baterai();
+    }
+    
+}
